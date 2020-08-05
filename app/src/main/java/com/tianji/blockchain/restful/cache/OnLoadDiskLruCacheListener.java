decc49462940d306ea8a641cf5bc7e2440337506
xugongpu <44052958+xugongpu@users.noreply.github.com>
2020-08-05 15:30:57 +0800

    08.05
    08.05

--- app/src/main/java/com/tianji/blockchain/restful/cache/OnLoadDiskLruCacheListener.java
+++
@@ -1,7 +0,0 @@
-package com.tianji.blockchain.restful.cache;
-
-import com.tianji.blockchainwallet.constant.enums.ResultCode;
-
-public interface OnLoadDiskLruCacheListener {
-    void load(ResultCode resultCode, String result);
-}
